b69b7e7b5b524e22dd58af44f35e7fcd8bbd080a
michal.skroban <michal.skroban@stxnext.pl>
2022-06-03 11:06:24 +0200

    syntax changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
-python manage.py migrate
 python manage.py collectstatic
+python manage.py migrate
+python manage.py migrate --run-syncdb
 gunicorn learnify_cfg.wsgi:application --bind 0.0.0.0:8000
